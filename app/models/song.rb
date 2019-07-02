10b08b2fde8faab2f35a362151bb21b6bda72cff
Mohammed Noori <kmanoorib@gmail.com>
2019-07-02 03:50:32 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -10,14 +10,13 @@ class Song < ApplicationRecord
   
   #  https://apidock.com/rails/Object/with_options
   # validates :content, length: { minimum: 50 }, if: -> { content.present? }
-  #with_options if: :released? do |song|
- #  song.validates :release_year, presence: true
- #  song.validates :release_year, numericality: {
- #    less_than_or_equal_to: Date.today.year}
- # end
+  with_options if: :released? do |song|
+   song.validates :release_year, presence: true
+   song.validates :release_year, numericality: {less_than_or_equal_to: Date.today.year}
+  end
 
-  #def released?
-  # released
-  #end
+  def released?
+   released
+  end
   
 end
